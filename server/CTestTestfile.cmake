246069f86c5572716e09af0e348793be7db831f2
Marlon Dias <marlonsdias@gmail.com>
2016-07-02 17:03:05 -0300

    adding c++ server

---
+++ server/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/msdias/Downloads/Simple-Web-Server-master
+# Build directory: /Users/msdias/Downloads/Simple-Web-Server-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(test)
