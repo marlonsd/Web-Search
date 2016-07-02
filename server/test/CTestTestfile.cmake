46f506a4a51602ec172aad97af4bfbd72a6a73f4
Marlon Dias <marlonsdias@gmail.com>
2016-07-02 17:09:28 -0300

    removing server

--- server/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/msdias/Documents/Universidade Federal de Minas Gerais/2016 1/Information Retrieval/TP3/Web-Search/server/test
-# Build directory: /Users/msdias/Documents/Universidade Federal de Minas Gerais/2016 1/Information Retrieval/TP3/Web-Search/server/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(parse_test "parse_test")
