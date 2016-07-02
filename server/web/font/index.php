651841cd9886f71b9c26faeb4cac4bbf2e155cb9
Marlon Dias <marlonsdias@gmail.com>
2016-07-02 19:07:10 -0300

    updating search page

---
+++ server/web/font/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>    
+  </head>
+  <body>    
+  </body>
+  <script type="text/javascript">
+      window.location = "../view/index.php";
+  </script>
+</html>
